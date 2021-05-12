167ad9c41c58705de8b91ab4adb9b79938d15488
Carletta Walls <carlettawalls88@gmail.com>
2021-05-11 21:41:56 -0400

    fixed the indentation on all files

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
-$(function() {
-  $(window).scroll(function() {
+$(function () {
+  $(window).scroll(function () {
     var winTop = $(window).scrollTop();
     if (winTop >= 30) {
       $("body").addClass("sticky-shrinknav-wrapper");
-    } else{
+    } else {
       $("body").removeClass("sticky-shrinknav-wrapper");
     }
   });
@@ -14,104 +14,104 @@ var searchBtn = document.getElementById("searchBtn")
 var userHistory = JSON.parse(localStorage.getItem('selection'))
 var clearBtn = document.getElementById('clearBtn');
 
-var setParams = function(inputs) {
-var userInput = inputs.value
-console.log(userInput);
-var searchArray = userInput.split(" ");
-console.log(searchArray);
-var searchParams = searchArray.join("%20");
-console.log(searchParams);
-return searchParams;
+var setParams = function (inputs) {
+  var userInput = inputs.value
+  console.log(userInput);
+  var searchArray = userInput.split(" ");
+  console.log(searchArray);
+  var searchParams = searchArray.join("%20");
+  console.log(searchParams);
+  return searchParams;
 };
 
-var geniusSearch = function(searchData) {
-const settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://genius.p.rapidapi.com/search?q=" + searchData,
-  "method": "GET",
-  "headers": {
-    "x-rapidapi-key": "ff5233b9b9mshd8cdfcef119429dp107763jsn3510bdb51ab2",
-    "x-rapidapi-host": "genius.p.rapidapi.com"
-  }
+var geniusSearch = function (searchData) {
+  const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://genius.p.rapidapi.com/search?q=" + searchData,
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "ff5233b9b9mshd8cdfcef119429dp107763jsn3510bdb51ab2",
+      "x-rapidapi-host": "genius.p.rapidapi.com"
+    }
+  };
+
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    var songResults = document.getElementById('songResults')
+    var songTitle = response['response']['hits'][0]['result']['full_title']
+    console.log(songTitle)
+
+    songResults.innerHTML = `${songTitle}`
+  });
 };
 
-  
- $.ajax(settings).done(function (response) {
-  console.log(response);
-  var songResults = document.getElementById('songResults')
-  var songTitle = response['response']['hits'][0]['result']['full_title']
-  console.log(songTitle)
+var deezerSearch = function (deezerData) {
+  const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + input.value,
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "65de35a1bamsh1f7acd97b19531ep117ad5jsn8557fcd005a4",
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+    }
+  };
 
-  songResults.innerHTML = `${songTitle}`
- });
-};
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    var deezerPre = response['data'][0]['preview'];
 
-var deezerSearch = function(deezerData){
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + input.value,
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "65de35a1bamsh1f7acd97b19531ep117ad5jsn8557fcd005a4",
-		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
-	}
-};
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-  var deezerPre = response['data'][0]['preview'];
-
-  var sound = document.createElement('audio');
-  sound.id = 'audio-player';
-  sound.controls = 'controls';
-  sound.src = deezerPre;
-  sound.type = 'audio/mpeg';
-  document.getElementById('song').appendChild(sound);
+    var sound = document.createElement('audio');
+    sound.id = 'audio-player';
+    sound.controls = 'controls';
+    sound.src = deezerPre;
+    sound.type = 'audio/mpeg';
+    document.getElementById('song').appendChild(sound);
 
 
 
-  
-})
+
+  })
 };
 
 
-$('#searchBtn').on('click', function(){
+$('#searchBtn').on('click', function () {
   geniusSearch(setParams(input));
   save();
   deezerSearch();
 });
 
-function save(){
+function save() {
   var newSelection = input.value
 
 
-if(localStorage.getItem('selection')== null){
+  if (localStorage.getItem('selection') == null) {
     localStorage.setItem('selection', '[]')
+  }
+
+  var oldSelection = JSON.parse(localStorage.getItem('selection'));
+  oldSelection.push(newSelection);
+
+  localStorage.setItem('selection', JSON.stringify(oldSelection));
+
+
 }
 
-var oldSelection = JSON.parse(localStorage.getItem('selection'));
-oldSelection.push(newSelection);
-
-localStorage.setItem('selection', JSON.stringify(oldSelection));
-
-
-}
 
 
 
+if (userHistory === null) {
 
-if(userHistory===null){
-
-}else{
+} else {
   for (let index = 0; index < userHistory.length; index++) {
     var a = document.createElement('li');
     var b = document.createTextNode(`${userHistory[index]}`);
     a.appendChild(b);
     document.querySelector('.list-group').appendChild(a);
     a.className += 'list-group-item';
-    
+
   }
 }
 
@@ -130,16 +130,16 @@ filterList();
 
 
 
-if(clearBtn === null){
+if (clearBtn === null) {
 
-}else{
-  clearBtn.addEventListener('click',function(){
+} else {
+  clearBtn.addEventListener('click', function () {
     localStorage.clear();
     document.getElementById('searchHistory').innerHTML = ""
   })
-  
+
 };
 
 
 
-    
+
